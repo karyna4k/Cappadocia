a48aebf713e91230fca173ee6ff03719766dc009
karyna4k <karyna.kananovich@gmail.com>
2020-06-04 18:35:11 +0300

    add 'fairy-tale' section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
+$(function() {
+    $('.fairy-tale__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 
 });
