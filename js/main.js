929a2d0b8fc21ece50ce84bbbb5dce8c57db7843
Karyna Kananovich <karyna.kananovich@gmail.com>
2020-06-08 18:51:04 +0300

    add @media

--- js/main.js
+++ js/main.js
@@ -2,15 +2,29 @@ $(function() {
     $('.fairy-tale__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-        autoplay: true,
-        fade: true
+        // autoplay: true,
+        fade: true,
+        responsive: [{
+                breakpoint: 601,
+                settings: {
+                    arrows: false
+                }
+            },
+
+        ]
     });
 
     $('.our-trip__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-        autoplay: true,
-        fade: true
+        // autoplay: true,
+        fade: true,
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false
+            }
+        }, ]
     });
 
 });
