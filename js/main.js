41fc432a2909406e6374b1acebc379a33223de4a
Karyna Kananovich <karyna.kananovich@gmail.com>
2020-06-10 22:19:03 +0300

    feat: add smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
     $('.fairy-tale__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
@@ -25,6 +26,19 @@ $(function() {
                 arrows: false
             }
         }, ]
+    });
+
+
+    // Smooth scroll
+
+    $(".menu, .our-trip__body").on("click", "a", function(event) {
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
     });
 
 });
